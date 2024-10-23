3095c2c4ba087abd21a9df787dc4af8b05ccf78f
Sascha Kiebler <sascha.kiebler@htwg-konstanz.de>
2024-10-23 16:57:57 +0200

    Refactor firestore.js to remove unnecessary code

--- backend/src/firestore.js
+++ backend/src/firestore.js
@@ -1,12 +1,8 @@
 // firestore.js
 var admin = require("firebase-admin");
 
-// Lade den privaten Schlüssel deines Service-Kontos
-const serviceAccount = require("../../../../../prvKeys/cloudServiceAccount.json");
-
 // Initialisiere Firebase
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin.firestore();
