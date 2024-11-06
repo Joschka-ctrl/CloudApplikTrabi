c626df28ecb32190a0d8131d6ae0edbe41a5f14b
Sascha Kiebler <sascha.kiebler@htwg-konstanz.de>
2024-11-06 16:21:15 +0100

    Add protected routes for user authentication in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import { auth } from "./firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useEffect, useState } from "react";
 import Register from "./pages/Register.js";
+import { Navigate } from 'react-router-dom';
 
 function App() {
 
@@ -26,21 +27,47 @@ function App() {
     setUser(null);
   };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<ContactSection />} />
-          <Route path="/defects" element={<Defects user={user ? user : null} />} />
-            <Route path="/login" element={<Login onLogin={setUser} />}></Route>
-            <Route path="/register" element={<Register onRegister={setUser} />}></Route>
-          </Routes>
-        </Router>
-      </header>
-    </div>
-  );
+  // Protected Route Component
+const ProtectedRoute = ({ user, children }) => {
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
+
+// Updated App component
+return (
+  <div className="App">
+    <header className="App-header">
+      <Router>
+        <Navbar user={user} onLogout={handleLogout} />
+        <Routes>
+          <Route path="/" element={<ContactSection />} />
+          <Route 
+            path="/defects" 
+            element={
+              <ProtectedRoute user={user}>
+                <Defects user={user} />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/login" 
+            element={
+              user ? <Navigate to="/" replace /> : <Login onLogin={setUser} />
+            }
+          />
+          <Route 
+            path="/register" 
+            element={
+              user ? <Navigate to="/" replace /> : <Register onRegister={setUser} />
+            }
+          />
+        </Routes>
+      </Router>
+    </header>
+  </div>
+);
 }
 
 export default App;
