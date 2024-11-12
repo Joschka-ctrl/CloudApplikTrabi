1c9cbe70cbe53d6e3790a29b92d3a5f8730f8482
Sascha Kiebler <sascha.kiebler@htwg-konstanz.de>
2024-11-12 16:19:38 +0100

    Remove Firebase configuration from firebase.js and initialize app without config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,13 +2,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
+/*
 const firebaseConfig = {
   apiKey: "AIzaSyCHuN2pr0uFCBl1Ud8yQOo0yn0gWANAUuA",
   authDomain: "trabantparking.firebaseapp.com",
   projectId: "trabantparking",
 };
+*/
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp();
 
 // Exportiere die Authentifizierungs-Instanz
 export const auth = getAuth(app);
