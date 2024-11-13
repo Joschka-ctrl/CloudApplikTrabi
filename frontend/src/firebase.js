565bfb078d52c54e2ba932b70c5c0165e47a8a01
Sascha Kiebler <sascha.kiebler@htwg-konstanz.de>
2024-11-13 08:58:18 +0100

    Update Firebase configuration to use environment variables for API key and project ID

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,15 +2,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-/*
-const firebaseConfig = {
-  apiKey: "AIzaSyCHuN2pr0uFCBl1Ud8yQOo0yn0gWANAUuA",
-  authDomain: "trabantparking.firebaseapp.com",
-  projectId: "trabantparking",
-};
-*/
 
-const app = initializeApp();
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_AUTH_API_KEY,
+  authDomain: process.env.REACT_APP_PROJECT_ID + ".firebaseapp.com",
+  projectId: process.env.REACT_APP_PROJECT_ID,
+};
+
+
+const app = initializeApp(firebaseConfig);
 
 // Exportiere die Authentifizierungs-Instanz
 export const auth = getAuth(app);
