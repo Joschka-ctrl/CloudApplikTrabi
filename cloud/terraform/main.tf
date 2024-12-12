315e006bac8f4f81466b26d1b353251e6a1086ac
BenKJI <bak00@web.de>
2024-12-12 19:01:52 +0100

    Added terraform and kubernetes yamls

---
+++ cloud/terraform/main.tf
@@ -0,0 +1,25 @@
+provider "google" {
+  project = "trabantparking-stage" # Replace with your project ID
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
+
+provider "kubernetes" {
+  host                   = google_container_cluster.gke.endpoint
+  client_certificate     = base64decode(google_container_cluster.gke.master_auth[0].client_certificate)
+  client_key             = base64decode(google_container_cluster.gke.master_auth[0].client_key)
+  cluster_ca_certificate = base64decode(google_container_cluster.gke.master_auth[0].cluster_ca_certificate)
+}
+
+resource "google_container_cluster" "gke" {
+  name     = "terraform-clustered"
+  location = "europe-west1"
+
+  deletion_protection = false
+
+  node_config {
+    machine_type = "e2-micro"
+  }
+
+  initial_node_count = 1
+}
