76197f4aa6a5903a8abfca34269a65e5052f56e9
Sascha Kiebler <sascha.kiebler@htwg-konstanz.de>
2025-01-04 11:55:38 +0100

    adjusted echarging service for auth

--- backend-echarging/src/server.js
+++ backend-echarging/src/server.js
@@ -5,9 +5,19 @@ const app = express();
 const router = express.Router();
 const port = 3016; // Different port from main backend
 
+if(process.env.NODE_ENV === 'development') {
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
 });
+}
+else {
+  const firebaseConfig = {
+    apiKey: process.env.AUTH_API_KEY,
+    authDomain: process.env.PROJECT_ID + ".firebaseapp.com",
+    projectId: process.env.PROJECT_ID,
+  };
+  admin.initializeApp(firebaseConfig);
+}
 
 const db = admin.firestore();
 
