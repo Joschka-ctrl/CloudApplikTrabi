ed36540b6c3ae6045cbaf5f169bf97fedd74f587
Sascha Kiebler <sascha.kiebler@htwg-konstanz.de>
2025-01-08 21:14:33 +0100

    again testing

--- cloud/terraform/main.tf
+++ cloud/terraform/main.tf
@@ -24,3 +24,32 @@ resource "google_container_cluster" "gke" {
   initial_node_count = 1
 }
 
+
+resource "google_storage_bucket" "default" {
+  name     = "trabantparking-stage-terraform-plans"
+  location = "europe-west1"
+
+  force_destroy               = false
+  public_access_prevention    = "enforced"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "local_file" "default" {
+  file_permission = "0644"
+  filename        = "${path.module}/backend.tf"
+
+  # You can store the template in a file and use the templatefile function for
+  # more modularity, if you prefer, instead of storing the template inline as
+  # we do here.
+  content = <<-EOT
+  terraform {
+    backend "gcs" {
+      bucket = "${google_storage_bucket.default.name}"
+    }
+  }
+  EOT
+}
