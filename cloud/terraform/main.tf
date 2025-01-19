c1955485c01d839019f0cc83d24d7dbc51a776b6
Joschka Peeters <joschka.peeters@gmail.com>
2025-01-19 17:03:59 +0100

    Update Firestore database location_id to use region instead of zone

--- cloud/terraform/main.tf
+++ cloud/terraform/main.tf
@@ -68,7 +68,7 @@ resource "local_file" "default" {
 
 resource "google_firestore_database" "firestore" {
   name        = var.cluster_name  # Der Name der Firestore-Datenbank
-  location_id = "europe-west1-b"    # Region der Firestore-Datenbank (z.B. "us-central1")
+  location_id = "europe-west1"    # Region der Firestore-Datenbank (z.B. "us-central1")
 
   type = "FIRESTORE_NATIVE"     # Typ der Firestore-Datenbank (FIRESTORE_NATIVE oder CLOUD_DATASTORE_COMPATIBILITY)
 }
