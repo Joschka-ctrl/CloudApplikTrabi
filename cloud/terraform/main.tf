0f4c59dfce52dd3fcdbb8554c9c8e665f504afd7
Joschka Peeters <joschka.peeters@gmail.com>
2025-01-20 19:13:47 +0100

    refactor: comment out unused local_file resource configuration in main.tf

--- cloud/terraform/main.tf
+++ cloud/terraform/main.tf
@@ -50,21 +50,21 @@ resource "google_container_cluster" "gke" {
 #   }
 # }
 
-resource "local_file" "default" {
-  file_permission = "0644"
-  filename        = "${path.module}/backend.tf"
+# resource "local_file" "default" {
+#   file_permission = "0644"
+#   filename        = "${path.module}/backend.tf"
 
-  # You can store the template in a file and use the templatefile function for
-  # more modularity, if you prefer, instead of storing the template inline as
-  # we do here.
-  # content = <<-EOT
-  # terraform {
-  #   backend "gcs" {
-  #     bucket = "${google_storage_bucket.default.name}"
-  #   }
-  # }
-  # EOT
-}
+#   # You can store the template in a file and use the templatefile function for
+#   # more modularity, if you prefer, instead of storing the template inline as
+#   # we do here.
+#   # content = <<-EOT
+#   # terraform {
+#   #   backend "gcs" {
+#   #     bucket = "${google_storage_bucket.default.name}"
+#   #   }
+#   # }
+#   # EOT
+# }
 
 resource "google_firestore_database" "firestore" {
   name        = var.cluster_name  # Der Name der Firestore-Datenbank
