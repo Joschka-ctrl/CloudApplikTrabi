e1f7e41da9343d2ef0b44691f74e2b0a26b07f5d
Joschka Peeters <joschka.peeters@gmail.com>
2025-01-20 19:12:32 +0100

    refactor: comment out unused Terraform backend configuration in main.tf and remove plan storage step in cluster-create-k8s.yml

--- cloud/terraform/main.tf
+++ cloud/terraform/main.tf
@@ -57,13 +57,13 @@ resource "local_file" "default" {
   # You can store the template in a file and use the templatefile function for
   # more modularity, if you prefer, instead of storing the template inline as
   # we do here.
-  content = <<-EOT
-  terraform {
-    backend "gcs" {
-      bucket = "${google_storage_bucket.default.name}"
-    }
-  }
-  EOT
+  # content = <<-EOT
+  # terraform {
+  #   backend "gcs" {
+  #     bucket = "${google_storage_bucket.default.name}"
+  #   }
+  # }
+  # EOT
 }
 
 resource "google_firestore_database" "firestore" {
