28d01d91c7dccc94e9848a656f6e13e18b509147
Joschka Peeters <joschka.peeters@gmail.com>
2025-01-20 19:16:22 +0100

    refactor: uncomment and restore Google Cloud Storage bucket and local file resource configurations in Terraform files

--- cloud/terraform/main.tf
+++ cloud/terraform/main.tf
@@ -32,39 +32,39 @@ resource "google_container_cluster" "gke" {
 }
 
 
-# resource "google_storage_bucket" "default" {
-#   name     = "trabantparking-stage-terraform-plans"
-#   location = "europe-west1"
+resource "google_storage_bucket" "default" {
+  name     = "trabantparking-stage-terraform-plans"
+  location = "europe-west1"
 
-#   force_destroy               = false
-#   public_access_prevention    = "enforced"
-#   uniform_bucket_level_access = true
+  force_destroy               = false
+  public_access_prevention    = "enforced"
+  uniform_bucket_level_access = true
 
-#   versioning {
-#     enabled = true
-#   }
+  versioning {
+    enabled = true
+  }
 
-#   lifecycle {
-#     prevent_destroy = true
-#     ignore_changes  = all
-#   }
-# }
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = all
+  }
+}
 
-# resource "local_file" "default" {
-#   file_permission = "0644"
-#   filename        = "${path.module}/backend.tf"
+resource "local_file" "default" {
+  file_permission = "0644"
+  filename        = "${path.module}/backend.tf"
 
-#   # You can store the template in a file and use the templatefile function for
-#   # more modularity, if you prefer, instead of storing the template inline as
-#   # we do here.
-#   # content = <<-EOT
-#   # terraform {
-#   #   backend "gcs" {
-#   #     bucket = "${google_storage_bucket.default.name}"
-#   #   }
-#   # }
-#   # EOT
-# }
+  # You can store the template in a file and use the templatefile function for
+  # more modularity, if you prefer, instead of storing the template inline as
+  # we do here.
+  content = <<-EOT
+  terraform {
+    backend "gcs" {
+      bucket = "${google_storage_bucket.default.name}"
+    }
+  }
+  EOT
+}
 
 resource "google_firestore_database" "firestore" {
   name        = var.cluster_name  # Der Name der Firestore-Datenbank
