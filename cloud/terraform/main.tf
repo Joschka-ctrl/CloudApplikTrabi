26bb711fe7e3ebaa9ea5b0653d27492a7aa6bdf5
Joschka Peeters <joschka.peeters@gmail.com>
2025-01-21 22:40:14 +0100

    feat: refactor cluster and node pool resources for improved configurability with region and zone variables

--- cloud/terraform/main.tf
+++ cloud/terraform/main.tf
@@ -18,30 +18,40 @@ resource "google_service_account" "default" {
   display_name = "continuous-integrator"
 }
 
-
-resource "google_container_cluster" "gke" {
+resource "google_container_cluster" "primary" {
   name     = var.cluster_name
-  location = "europe-west1-b"
+  location = var.zone
 
-  deletion_protection = false
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
 
-  addons_config {
-    http_load_balancing {
-      disabled = false
-    }
-  }
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = var.cluster_name
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
 
   node_config {
+    preemptible  = true
+     
     machine_type = "e2-small"
     disk_size_gb = 30
-  }
 
-  initial_node_count = 1
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
 
 resource "google_storage_bucket" "default" {
   name     = "trabantparking-stage-terraform-plans"
-  location = "europe-west1"
+  location = var.region
 
   force_destroy               = false
   public_access_prevention    = "enforced"
@@ -75,11 +85,10 @@ resource "local_file" "default" {
 
 resource "google_firestore_database" "firestore" {
   name        = var.cluster_name  # Der Name der Firestore-Datenbank
-  location_id = "europe-west1"    # Region der Firestore-Datenbank (z.B. "us-central1")
+  location_id = var.region    # Region der Firestore-Datenbank (z.B. "us-central1")
 
   type = "FIRESTORE_NATIVE"     # Typ der Firestore-Datenbank (FIRESTORE_NATIVE oder CLOUD_DATASTORE_COMPATIBILITY)
 }
-
 
 output "firestore_database_id" {
   value = google_firestore_database.firestore.id
