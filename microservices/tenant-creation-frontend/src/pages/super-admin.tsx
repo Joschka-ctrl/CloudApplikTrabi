f51fb24614b3f981d372ed293253276baa5d3ba9
Sascha Kiebler <sascha.kiebler@htwg-konstanz.de>
2025-01-23 10:34:24 +0100

    removed unused imports

--- microservices/tenant-creation-frontend/src/pages/super-admin.tsx
+++ microservices/tenant-creation-frontend/src/pages/super-admin.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import {
   Container,
   Typography,
   Paper,
-  Box,
   Table,
   TableBody,
   TableCell,
