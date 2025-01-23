285fe15a73d0cac888a6f31dfc0f84d7104c9a8d
Sascha Kiebler <sascha.kiebler@htwg-konstanz.de>
2025-01-23 12:57:37 +0100

    use master workflow

--- microservices/backend-tenant-creation/server.js
+++ microservices/backend-tenant-creation/server.js
@@ -59,14 +59,14 @@ async function triggerWorkflow(tenantConfig) {
     console.log('Owner:', process.env.GITHUB_OWNER);
     console.log('Repo:', process.env.GITHUB_REPO);
     console.log('Workflow ID:', 'cluster-create-k8s.yml');
-    console.log('Branch/Ref:', 'stage-cluster');
+    console.log('Branch/Ref:', 'master');
     console.log('Inputs:', { tenant_name: tenantConfig.tenantName });
 
     await octokit.actions.createWorkflowDispatch({
       owner: process.env.GITHUB_OWNER,
       repo: process.env.GITHUB_REPO,
       workflow_id: 'cluster-create-k8s.yml',
-      ref: 'stage-cluster',
+      ref: 'master',
       inputs: {
         tenant_name: tenantConfig.tenantName,
       },
@@ -86,14 +86,14 @@ async function triggerDeleteWorkflow(tenantId) {
     console.log('Owner:', process.env.GITHUB_OWNER);
     console.log('Repo:', process.env.GITHUB_REPO);
     console.log('Workflow ID:', 'destroy.yml');
-    console.log('Branch/Ref:', 'stage');
+    console.log('Branch/Ref:', 'master');
     console.log('Inputs:', { tenant_name: tenantId });
 
     await octokit.actions.createWorkflowDispatch({
       owner: process.env.GITHUB_OWNER,
       repo: process.env.GITHUB_REPO,
       workflow_id: 'destroy.yml',
-      ref: 'stage',
+      ref: 'master',
       inputs: {
         tenant_name: tenantId,
       },
@@ -113,7 +113,7 @@ async function triggerStopWorkflow() {
       owner: process.env.GITHUB_OWNER,
       repo: process.env.GITHUB_REPO,
       workflow_id: 'shutdown-stage.yml',
-      ref: 'stage',
+      ref: 'master',
       inputs: {
         region: "europe-west1",
         zone: "europe-west1-c",
